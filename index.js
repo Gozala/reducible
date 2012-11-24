78c2316f374543eb862047c65821a2ff46fbc5b0
Irakli Gozalishvili <rfobic@gmail.com>
2012-11-23 19:05:05 -0800

    Extract reducers core into separate package.

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+exports.accumulate = require("./accumulate")
+exports.end = require("./end")
+exports.reduced = require("./reduced")
+exports.isReduced = require("./is-reduced")
+exports.isError = require("./is-error")
---
+++ test/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+exports["test reduce"] = require("./reduce")
+
+require("test").run(exports)
