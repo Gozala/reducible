fae406ab4df4df3e12d49f1099505ab9eabee3d3
Raynos <raynos2@gmail.com>
2013-01-20 17:20:02 -0800

    error test

--- test/error.js
+++ test/error.js
@@ -2,6 +2,7 @@
 
 var reduce = require("../reduce")
 var reducible = require("../reducible")
+var isError = require("../is-error")
 var end = require("../end")
 var reduced = require("../reduced")
 
@@ -59,6 +60,10 @@ exports["test thrown errors with reducible"] = function (assert) {
 
   var toArray = function (source, callback) {
     reduce(source, function (value, buffer) {
+      if (isError(value)) {
+        return callback(value)
+      }
+
       called++
       assert.ok(!(value instanceof Error), "value is not an Error")
       if (value === end) {
@@ -80,15 +85,33 @@ exports["test thrown errors with reducible"] = function (assert) {
   assert.equal(called, 4, "toArray is called 4 times")
   assert.equal(callbackCalled, 1, "callback is called once")
 
+  var first = false
+  var second = false
+
   assert.throws(function () {
     toArray(list([1,2,3]), function (err, result) {
+      if (first) {
+        first = false
+        second = true
+
+        assert.equal(err.message, "Some Error", "first error is passed back to me")
+      } else if (second) {
+
+        assert.equal(err.message, "Some Error", "second error is passed back to me")
+        second = "finished"
+      } else {
+        first = true
+      }
+
       callbackCalled++
       throw new Error("Some Error")
     })
-  }, /Some Error/, "Some Error is thrown")
+  }, /Some Error/, "Some Error get's thrown the third time")
 
+  assert.equal(first, false, "first was handled")
+  assert.equal(second, "finished", "second was handled")
   assert.equal(called, 8, "toArray is called 8 times")
-  assert.equal(callbackCalled, 2, "callback is called twice")
+  assert.equal(callbackCalled, 4, "callback is called four times")
 }
 
 if (require.main === module)
